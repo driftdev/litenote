a59b17f05f38418c73cc282532e9c983c954bc44
ArkamFahry <92285161+ArkamFahry@users.noreply.github.com>
2024-01-04 15:11:07 +0530

    feat: added provider based simple state management

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:notelite/pages/notes_page.dart';
+import 'package:provider/provider.dart';
 import 'package:notelite/services/database/note_database.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await NoteDatabase.init();
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => NoteDatabase(),
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
